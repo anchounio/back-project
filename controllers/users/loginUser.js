397cf398055895b3271141f79ce17d82c9e7f45e
anchounio <toni.clemente.garcia@gmail.com>
2022-06-06 13:29:08 +0200

    Improvements on login

--- controllers/users/loginUser.js
+++ controllers/users/loginUser.js
@@ -4,43 +4,46 @@ const selectUserByEmailQuery = require('../../db/userQueries/selectUserByEmailQu
 const { generateError } = require('../../helpers');
 
 const loginUser = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    if (!email || !password) {
-      throw generateError('Faltan campos', 400);
+        if (!email || !password) {
+            throw generateError('Faltan campos', 400);
+        }
+
+        // obtenemos el usuario con el mail del body
+        const user = await selectUserByEmailQuery(email);
+
+        console.log(user);
+        // comprobamos que las contraseñas coinciden
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        console.log(password);
+        console.log(user.password);
+        // si no coinciden, error
+        if (!validPassword) {
+            throw generateError('Contraseña incorrecta', 401);
+        }
+
+        // información que queremos guardar en el token
+        const payload = {
+            id: user.id,
+        };
+
+        // firmamos el token
+        const token = jwt.sign(payload, process.env.SECRET, {
+            expiresIn: '30d',
+        });
+
+        res.send({
+            status: 'ok',
+            data: {
+                token,
+            },
+        });
+    } catch (err) {
+        next(err);
     }
-
-    // obtenemos el usuario con el mail del body
-    const user = await selectUserByEmailQuery(email);
-
-    // comprobamos que las contraseñas coinciden
-    const validPassword = await bcrypt.compare(password, user.password);
-
-    // si no coinciden, error
-    if (!validPassword) {
-      throw generateError('Contraseña incorrecta', 401);
-    }
-
-    // información que queremos guardar en el token
-    const payload = {
-      id: user.id,
-    };
-
-    // firmamos el token
-    const token = jwt.sign(payload, process.env.SECRET, {
-      expiresIn: '30d',
-    });
-
-    res.send({
-      status: 'ok',
-      data: {
-        token,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
 };
 
 module.exports = loginUser;
--- db/userQueries/selectUserByEmailQuery.js
+++ db/userQueries/selectUserByEmailQuery.js
@@ -2,27 +2,27 @@ const { generateError } = require('../../helpers');
 const getConnection = require('../getConnection');
 
 const selectUserByEmailQuery = async (email) => {
-  let connection;
+    let connection;
 
-  try {
-    connection = await getConnection();
+    try {
+        connection = await getConnection();
 
-    // se crea el array de usuarios con ese email
-    const [users] = await connection.query(
-      `SELECT id, name FROM users WHERE email = ?`,
-      [email]
-    );
+        // se crea el array de usuarios con ese email
+        const [users] = await connection.query(
+            `SELECT id, email, password FROM users WHERE email = ?`,
+            [email]
+        );
 
-    // si no existe, error
-    if (users.length < 1) {
-      throw generateError('Usuario no encontrado', 404);
+        // si no existe, error
+        if (users.length < 1) {
+            throw generateError('Usuario no encontrado', 404);
+        }
+
+        // se devuelve el usuario en la posición 0
+        return users[0];
+    } finally {
+        if (connection) connection.release();
     }
-
-    // se devuelve el usuario en la posición 0
-    return users[0];
-  } finally {
-    if (connection) connection.release();
-  }
 };
 
 module.exports = selectUserByEmailQuery;
