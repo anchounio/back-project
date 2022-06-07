f3f3243a63bb2074de167120e5f4aad0e3540406
danielortegaperez <ortegaperezdaniel@gmail.com>
2022-06-07 12:43:44 +0200

    modified updateExerciseQuery

--- db/exercisesQueries/updateExerciseQuery.js
+++ db/exercisesQueries/updateExerciseQuery.js
@@ -13,12 +13,45 @@ const updateExerciseQuery = async (
     try {
         connection = await getConnection();
 
-        await connection.query(
-            `UPDATE exercises 
-            SET name = ?, description = ?, typology = ?, muscularGroup = ?, photo = ?
-            WHERE id = ?`,
-            [name, description, typology, muscularGroup, photo, idExercise]
-        );
+        if (name) {
+            await connection.query(
+                `UPDATE exercises 
+                SET name = ? WHERE id = ?`,
+                [name, idExercise]
+            );
+        }
+
+        if (description) {
+            await connection.query(
+                `UPDATE exercises 
+                SET description = ? WHERE id = ?`,
+                [description, idExercise]
+            );
+        }
+
+        if (typology) {
+            await connection.query(
+                `UPDATE exercises 
+                SET typology = ? WHERE id = ?`,
+                [typology, idExercise]
+            );
+        }
+
+        if (muscularGroup) {
+            await connection.query(
+                `UPDATE exercises 
+                SET muscularGroup = ? WHERE id = ?`,
+                [muscularGroup, idExercise]
+            );
+        }
+
+        if (photo) {
+            await connection.query(
+                `UPDATE exercises 
+                SET photo = ? WHERE id = ?`,
+                [photo, idExercise]
+            );
+        }
     } finally {
         if (connection) connection.release();
     }
